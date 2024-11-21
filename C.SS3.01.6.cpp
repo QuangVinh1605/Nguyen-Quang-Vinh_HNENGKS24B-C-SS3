78620e60f04cd0f5b40f4b379bb3dae51eb06c31
QuangVinh1605 <nhv16052006@gmail.com>
2024-11-21 22:44:54 +0700

    upload baitap C ss3-01

---
+++ C.SS3.01.6.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+	float C;
+	printf ("hay nhap vao nhiet do ma ban muon doi sang do F") ;
+	scanf("%.2f",&C);
+	float F=(C*9/5)+32;
+	printf ("%.2f do C= %.2f do F/n",C,F);
+	return 0; 
+	
+} 
